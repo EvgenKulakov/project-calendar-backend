932241d678f2085c55ee2dd75fc4be5d8c48e012
Evgeniy Kulakov <000evgeniykulakov@gmail.com>
2024-06-02 14:59:32 +0300

    init commit

---
+++ src/main/java/com/itssamara/project/calendar/backend/App.java
@@ -0,0 +1,13 @@
+package com.itssamara.project.calendar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
